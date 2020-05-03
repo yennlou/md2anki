0ec4e88da0a5f6373375d7a802bdb0a85580e591
“Yenn <yennlou21@gmail.com>
2020-05-03 21:22:10 +1200

    split md file

--- src/index.ts
+++ src/index.ts
@@ -2,11 +2,39 @@ import unified = require('unified')
 import markdown = require('remark-parse')
 import remark2rehype = require('remark-rehype')
 import html = require('rehype-stringify')
+import { MOCK_DATA } from './mock'
+import { Parent, Node } from 'unist'
 
-unified()
-  .use(markdown)
-  .use(remark2rehype)
-  .use(html)
-  .process('# Hello', (err, file) => {
-    console.log(String(file))
-  })
+const mast = unified().use(markdown).parse(MOCK_DATA) as Parent
+const nodes = mast.children
+
+let buffer: Node[] = []
+let newTrees: Node[] = []
+
+const flushable = (buffer: Node[]) => {
+  if (buffer.length && buffer[buffer.length - 1].type !== 'heading') return true
+  return false
+}
+
+const flush = (buffer: Node[], newTrees) => {
+  newTrees.push({ type: 'root', children: [...buffer] })
+  buffer.length = 0
+}
+
+for (const node of nodes) {
+  if (node.type === 'heading' && node.depth < 4) {
+    if (flushable(buffer)) flush(buffer, newTrees)
+  }
+  buffer.push(node)
+}
+
+if (flushable(buffer)) flush(buffer, newTrees)
+
+newTrees.map((mast) => {
+  const hast = unified().use(remark2rehype).runSync(mast)
+  console.log(unified().use(html).stringify(hast))
+  console.log('======')
+})
+
+// const hast = unified().use(remark2rehype).runSync(mast)
+// console.log(unified().use(html).stringify(hast))
---
+++ src/mock.ts
@@ -0,0 +1,20 @@
+export const MOCK_DATA = `
+  # Hello World
+
+  ## Table of Content
+
+  aaa
+
+  ## Install
+
+  A **example**.
+
+  ## Use
+
+  More \`text\`.
+
+  ## License
+
+  MIT
+
+`
